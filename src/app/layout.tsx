be7a078e01dec5c444743465d95db8b9f1ad52ef
choccy2 <kujoxd6@gmail.com>
2024-09-20 22:26:32 +0530

    1.0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <title>STYLIAN</title>
       <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet"/>
      
       </head>
